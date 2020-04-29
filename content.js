2aaf74c3f60c5cc55d8925734849e04d5b5cdf0c
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2020-04-29 15:39:08 +0000

    listen for window location url changes
    Only remove the background image if 'hot-pocket' is in the URL

--- content.js
+++ content.js
@@ -1,3 +1,31 @@
-const root = document.querySelector('#trello-root');
-root.style = '';
-root.style.backgroundColor = 'lightsteelblue';
+function removeBackgroundImage() {
+  if (/hot-pocket/.test(document.location.href)) {
+    const root = document.querySelector('#trello-root');
+    root.style = '';
+    root.style.backgroundColor = 'lightsteelblue';
+  }
+}
+
+window.onload = function() {
+  removeBackgroundImage();
+
+  var oldHref = document.location.href;
+  var bodyList = document.querySelector('body');
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (oldHref != document.location.href) {
+        oldHref = document.location.href;
+        removeBackgroundImage();
+      }
+    });
+  });
+
+  var config = {
+    childList: true,
+    subtree: true
+  };
+
+  observer.observe(bodyList, config);
+}
+
+removeBackgroundImage();
